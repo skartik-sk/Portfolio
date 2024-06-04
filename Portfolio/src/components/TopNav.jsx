42c9ea1bde7e49855d7a356f9ce05a3061b006a5
Singupalli Kartik <skartik1706@gmail.com>
2024-06-04 15:54:48 +0530

    chore: Add Vite configuration and initial files for React project

---
+++ Portfolio/src/components/TopNav.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import {logo} from '../assets/logo.png'
+const TopNav = () => {
+  return (
+    <div className='w-full'>
+        <div>
+            <div>
+                <div>
+                    <img src="https://github.com/SingupalliKartik/Portfolio/blob/main/Portfolio/src/assets/logo.png"alt="" />
+                </div>
+
+                <div>
+                    <div>Home</div>
+                    <div>Services</div>
+                    <div>Projects</div>
+                    <div>About</div>
+                    <div>Blog</div>
+
+                </div>
+
+            </div>
+
+            <div>
+                <button>Let's Talk</button>
+            </div>
+        </div>
+        
+    </div>
+  )
+}
+
+export default TopNav
---
+++ Portfolio/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: theme => ({
+        'gradient-background': 'linear-gradient(90deg, #b16cea 14.06%, #ff5e69 44.79%, #ff8a56 71.88%, #ffa84b)',
+      })
+,
+      colors:{
+        'Black-primary': '#161513',
+        'Black-secondary':'#1C1C22',
+        'White-primary':'#F0F2F5',
+        'White-secondary':'#ffffff',
+      },
+     
+    },
+  },
+  plugins: [],
+}
+
