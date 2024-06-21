007756cb3719806cd5fb9b08e2499a6000cd54ad
Singupalli Kartik <skartik1706@gmail.com>
2024-06-21 22:32:11 +0530

    chore: Remove commented out code and unused imports in project.jsx

---
+++ Portfolio/src/project.jsx
@@ -0,0 +1,15 @@
+// import * as React from "react";
+// import { render } from "react-dom";
+// import { Header } from "./Header";
+// import { CardList } from "./CardList";
+
+// // import "./styles.css";
+
+// export default function Projects() {
+//   return (
+//     <div className="container">
+//       {/* <Header /> */}
+//       {/* <CardList /> */}
+//     </div>
+//   );
+// }
