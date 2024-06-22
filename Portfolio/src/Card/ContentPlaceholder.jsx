d4279d673817cd185a8ace14588e8b1408c30f61
Singupalli Kartik <skartik1706@gmail.com>
2024-06-22 08:36:02 +0530

    chore: Remove commented out code and unused imports in ContentPlaceholder.jsx

---
+++ Portfolio/src/Card/ContentPlaceholder.jsx
@@ -0,0 +1,15 @@
+// import * as React from "react";
+// //import { LoremIpsum } from "react-lorem-ipsum";
+// import { motion, useDeprecatedInvertedScale } from "framer-motion";
+
+// export const ContentPlaceholder = React.memo(() => {
+//   const inverted = useDeprecatedInvertedScale();
+//   return (
+//     <motion.div
+//       className="content-container"
+//       style={{ ...inverted, originY: 0, originX: 0 }}
+//     >
+//       <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta consectetur non quibusdam, dolorum atque maxime. Vero deserunt eius ratione facilis repudiandae accusamus, nihil id. Ratione ad eaque tempore architecto illo?</div>
+//     </motion.div>
+//   );
+// });
