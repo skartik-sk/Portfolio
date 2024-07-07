b9b76e3dd2ebeb5f6b5acf669a643832d0100d24
Singupalli Kartik <skartik1706@gmail.com>
2024-07-07 09:02:30 +0530

    Refactor CardList component to remove commented out code and unused imports

--- Portfolio/src/Card/ContentPlaceholder.jsx
+++ Portfolio/src/Card/ContentPlaceholder.jsx
@@ -1,15 +1,17 @@
-// import * as React from "react";
-// //import { LoremIpsum } from "react-lorem-ipsum";
-// import { motion, useDeprecatedInvertedScale } from "framer-motion";
+import * as React from "react";
+//import { LoremIpsum } from "react-lorem-ipsum";
+import { motion, useDeprecatedInvertedScale } from "framer-motion";
 
-// export const ContentPlaceholder = React.memo(() => {
-//   const inverted = useDeprecatedInvertedScale();
-//   return (
-//     <motion.div
-//       className="content-container"
-//       style={{ ...inverted, originY: 0, originX: 0 }}
-//     >
-//       <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta consectetur non quibusdam, dolorum atque maxime. Vero deserunt eius ratione facilis repudiandae accusamus, nihil id. Ratione ad eaque tempore architecto illo?</div>
-//     </motion.div>
-//   );
-// });
+export const ContentPlaceholder = React.memo(() => {
+  const inverted = useDeprecatedInvertedScale();
+  return (
+    <motion.div
+      className="p-[460px_35px_35px]"
+      style={{ ...inverted, originY: 0, originX: 0 }}
+    >
+      <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta consectetur non quibusdam, dolorum atque maxime. Vero deserunt eius ratione facilis repudiandae accusamus, nihil id. Ratione ad eaque tempore architecto illo?</div>
+      <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta consectetur non quibusdam, dolorum atque maxime. Vero deserunt eius ratione facilis repudiandae accusamus, nihil id. Ratione ad eaque tempore architecto illo?</div>
+      <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta consectetur non quibusdam, dolorum atque maxime. Vero deserunt eius ratione facilis repudiandae accusamus, nihil id. Ratione ad eaque tempore architecto illo?</div>
+    </motion.div>
+  );
+});
--- Portfolio/src/project.jsx
+++ Portfolio/src/project.jsx
@@ -1,15 +1,12 @@
-// import * as React from "react";
-// import { render } from "react-dom";
-// import { Header } from "./Header";
-// import { CardList } from "./CardList";
+import * as React from "react";
+import { CardList } from "./CardList";
 
-// // import "./styles.css";
+// import "./styles.css";
 
-// export default function Projects() {
-//   return (
-//     <div className="container">
-//       {/* <Header /> */}
-//       {/* <CardList /> */}
-//     </div>
-//   );
-// }
+export default function Projects() {
+  return (
+    <div className="max-w-screen-lg flex-auto p-[45px_25px]">
+      <CardList />
+    </div>
+  );
+}
