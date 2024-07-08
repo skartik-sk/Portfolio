107c93e35f2846010e8175cc2c8fe19d3bf1a599
Singupalli Kartik <skartik1706@gmail.com>
2024-07-08 07:34:12 +0530

    Refactor Image component to use absolute positioning for better layout control

--- Portfolio/src/Card/Image.jsx
+++ Portfolio/src/Card/Image.jsx
@@ -1,30 +1,30 @@
-// import * as React from "react";
-// import { motion, useDeprecatedInvertedScale } from "framer-motion";
-// import { closeSpring } from "./animations";
+import * as React from "react";
+import { motion, useDeprecatedInvertedScale } from "framer-motion";
+import { closeSpring } from "./animations";
 
-// export const Image = ({
-//   id,
-//   isSelected,
-//   pointOfInterest = 0,
-//   backgroundColor
-// }) => {
-//   const inverted = useDeprecatedInvertedScale();
+export const Image = ({
+  id,
+  isSelected,
+  pointOfInterest = 0,
+  backgroundColor
+}) => {
+  const inverted = useDeprecatedInvertedScale();
 
-//   return (
-//     <motion.div
-//       className="card-image-container"
-//       style={{ ...inverted, backgroundColor, originX: 0, originY: 0 }}
-//     >
-//       <motion.img
-//         className="card-image"
-//         src={`images/${id}.jpg`}
-//         alt=""
-//         initial={false}
-//         animate={
-//           isSelected ? { x: -20, y: -20 } : { x: -pointOfInterest, y: 0 }
-//         }
-//         transition={closeSpring}
-//       />
-//     </motion.div>
-//   );
-// };
+  return (
+    <motion.div
+      className="absolute top-0 left-0 overflow-hidden h-[420px] w-screen"
+      style={{ ...inverted, backgroundColor, originX: 0, originY: 0 }}
+    >
+      <motion.img
+        className="card-image"
+        src={`images/${id}.jpg`}
+        alt=""
+        initial={false}
+        animate={
+          isSelected ? { x: -20, y: -20 } : { x: -pointOfInterest, y: 0 }
+        }
+        transition={closeSpring}
+      />
+    </motion.div>
+  );
+};
--- Portfolio/src/Card/index.jsx
+++ Portfolio/src/Card/index.jsx
@@ -1,108 +1,121 @@
-// import * as React from "react";
-// import { memo, useRef } from "react";
-// import { motion, useMotionValue } from "framer-motion";
-// import { Link } from "react-router-dom";
-// import { useInvertedBorderRadius } from "../utils/use-inverted-border-radius";
-// //import { CardData } from "../types";
-// import { ContentPlaceholder } from "./ContentPlaceholder";
-// import { Title } from "./Title";
-// import { Image } from "./Image";
-// import { openSpring, closeSpring } from "./animations";
-// import { useScrollConstraints } from "../utils/use-scroll-constraints";
-// import { useWheelScroll } from "../utils/use-wheel-scroll";
+import * as React from "react";
+import { memo, useRef,useState } from "react";
+import { motion, useMotionValue, useMotionValueEvent, useScroll } from "framer-motion";
+import { Link, useNavigate } from "react-router-dom";
+import { useInvertedBorderRadius } from "../utils/use-inverted-border-radius";
+//import { CardData } from "../types";
+import { ContentPlaceholder } from "./ContentPlaceholder";
+import { Title } from "./Title";
+import { Image } from "./Image";
+import { openSpring, closeSpring } from "./animations";
+import { useScrollConstraints } from "../utils/use-scroll-constraints";
+import { useWheelScroll } from "../utils/use-wheel-scroll";
 
-// // interface Props extends CardData {
-// //   isSelected: boolean;
-// //   history: {
-// //     push: (route: string) => void;
-// //   };
-// // }
+// interface Props extends CardData {
+//   isSelected: boolean;
+//   history: {
+//     push: (route: string) => void;
+//   };
+// }
 
-// // Distance in pixels a user has to scroll a card down before we recognise
-// // a swipe-to dismiss action.
-// const dismissDistance = 150;
+// Distance in pixels a user has to scroll a card down before we recognise
+// a swipe-to dismiss action.
+const dismissDistance = 100 ;
 
-// export const Card = memo(
-//   ({
-//     isSelected,
-//     id,
-//     title,
-//     category,
-//     history,
-//     pointOfInterest,
-//     backgroundColor
-//   }) => {
-//     const y = useMotionValue(0);
-//     const zIndex = useMotionValue(isSelected ? 2 : 0);
+export const Card = memo(
+  ({
+    isSelected,
+    id,
+    title,
+    category,
+    navigate,
+    pointOfInterest,
+    backgroundColor
+  }) => {
+    // const navigate = useNavigate();
+    const y = useMotionValue(0);
+    const x = useMotionValue(0);
+    const zIndex = useMotionValue(isSelected ? 2 : 0);
 
-//     // Maintain the visual border radius when we perform the layoutTransition by inverting its scaleX/Y
-//     const inverted = useInvertedBorderRadius(20);
+    // Maintain the visual border radius when we perform the layoutTransition by inverting its scaleX/Y
+    const inverted = useInvertedBorderRadius(20);
 
-//     // We'll use the opened card element to calculate the scroll constraints
-//     const cardRef = useRef(null);
-//     const constraints = useScrollConstraints(cardRef, isSelected);
+    // We'll use the opened card element to calculate the scroll constraints
+    const cardRef = useRef(null);
+    const constraints = useScrollConstraints(cardRef, isSelected);
+console.log("value :" +constraints.bottom);
+    function checkSwipeToDismiss() {
+     if (y.get() > dismissDistance) {
+      isSelected= !isSelected
+      navigate("/");
+  }
+    }
 
-//     function checkSwipeToDismiss() {
-//       y.get() > dismissDistance && history.push("/");
-//     }
+    function checkZIndex(latest) {
+      if (isSelected) {
+        zIndex.set(2);
+      } else if (!isSelected && latest.scaleX < 1.01) {
+        zIndex.set(0);
+      }
+    }
 
-//     function checkZIndex(latest) {
-//       if (isSelected) {
-//         zIndex.set(2);
-//       } else if (!isSelected && latest.scaleX < 1.01) {
-//         zIndex.set(0);
-//       }
-//     }
+    // When this card is selected, attach a wheel event listener
+    const containerRef = useRef(null);
+    useWheelScroll(
+      containerRef,
+      y,
+      constraints,
+      checkSwipeToDismiss,
+      isSelected
+    );
 
-//     // When this card is selected, attach a wheel event listener
-//     const containerRef = useRef(null);
-//     useWheelScroll(
-//       containerRef,
-//       y,
-//       constraints,
-//       checkSwipeToDismiss,
-//       isSelected
-//     );
+useMotionValueEvent (x,
+  "change",
+  (latest) => {
+   x.set(latest)
+   console.log(x);
+    // setscx(latest)
+  }) ;
 
-//     return (
-//       <li ref={containerRef} className={`card`}>
-//         <Overlay isSelected={isSelected} />
-//         <div className={`card-content-container ${isSelected && "open"}`}>
-//           <motion.div
-//             ref={cardRef}
-//             className="card-content"
-//             style={{ ...inverted, zIndex, y }}
-//             layoutTransition={isSelected ? openSpring : closeSpring}
-//             drag={isSelected ? "y" : false}
-//             dragConstraints={constraints}
-//             onDrag={checkSwipeToDismiss}
-//             onUpdate={checkZIndex}
-//           >
-//             <Image
-//               id={id}
-//               isSelected={isSelected}
-//               pointOfInterest={pointOfInterest}
-//               backgroundColor={backgroundColor}
-//             />
-//             <Title title={title} category={category} isSelected={isSelected} />
-//             <ContentPlaceholder />
-//           </motion.div>
-//         </div>
-//         {!isSelected && <Link to={id} className={`card-open-link`} />}
-//       </li>
-//     );
-//   },
-//   (prev, next) => prev.isSelected === next.isSelected
-// );
+    return (
+      <div ref={containerRef} className={` relative p-6 h-[460px] flex-none w-4/12`}>
+        <Overlay isSelected={isSelected} />
+        <div className={` ${isSelected ? `fixed top-0 left-${scx}  right-6  z-10 overflow-hidden p-0` : "block relative w-fit h-full pointer-events-none"}`}>
+          <motion.div
+            ref={cardRef}
+            className=" overflow-hidden pointer-events-auto relative rounded-xl bg-[#1c1c1e]  h-full m-auto"
+            style={{ ...inverted, zIndex, y }}
+            layoutTransition={isSelected ? openSpring : closeSpring}
+            drag={isSelected ? "y" : false}
+            dragConstraints={constraints}
+            onDrag={checkSwipeToDismiss}
+            onUpdate={checkZIndex}
+          >
+            <Image
+              id={id}
+              isSelected={isSelected}
+              pointOfInterest={pointOfInterest}
+              backgroundColor={backgroundColor}
+            />
+            <Title title={title} category={category} isSelected={isSelected} />
+            <ContentPlaceholder />
+          </motion.div>
+        </div>
+        {!isSelected && <Link to={id} className={`absolute inset-0`} />}
+      </div>
+    );
+  },
+  (prev, next) => prev.isSelected === next.isSelected
+);
 
-// const Overlay = ({ isSelected }) => (
-//   <motion.div
-//     initial={false}
-//     animate={{ opacity: isSelected ? 1 : 0 }}
-//     transition={{ duration: 0.2 }}
-//     style={{ pointerEvents: isSelected ? "auto" : "none" }}
-//     className="overlay"
-//   >
-//     <Link to="/" />
-//   </motion.div>
-// );
+const Overlay = ({ isSelected }) => (
+  <motion.div
+    initial={false}
+    animate={{ opacity: isSelected ? 1 : 0 }}
+    transition={{ duration: 0.2 }}
+    style={{ pointerEvents: isSelected ? "auto" : "none" }}
+    className="fixed z-10 bg-black/80  top-0 bottom-0  w-screen h-screen translate-x-[-50%]"
+  >
+    <Link  to="/" />
+  </motion.div>
+);
