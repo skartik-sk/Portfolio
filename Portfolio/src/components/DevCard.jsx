21bc933de23c3ad043b42601739491a92016bf8f
Singupalli Kartik <skartik1706@gmail.com>
2024-07-09 21:37:45 +0530

    feat: Add Android, React, Flutter, and UI/UX services to DevCard component

--- Portfolio/src/components/DevCard.jsx
+++ Portfolio/src/components/DevCard.jsx
@@ -1,24 +1,30 @@
 import React from "react";
 import { FiCreditCard, FiMail, FiUser, FiUsers } from "react-icons/fi";
-
+import { IoLogoAndroid } from "react-icons/io";
+import { SiFlutter } from "react-icons/si";
+import { IoLogoReact } from "react-icons/io5";
+import { IoLogoFigma } from "react-icons/io5";
 const HoverDevCards = () => {
   return (
-    <div className="px-14  ">
-      <p className="text-xl font-semibold mb-2">Settings</p>
-      <div className="grid gap-4 grid-cols-2 lg:grid-cols-4">
+    <div className="px-16  ">
+    <span className="text-transparent px-14  text-4xl font-extrabold pb-3    bg-clip-text bg-gradient-background">
+            
+              Services
+            </span>
+      <div className="grid gap-4 py-6 grid-cols-2 lg:grid-cols-4">
         <Card
-          title="Account"
-          subtitle="Manage profile"
+          title="Android"
+          subtitle="Android app Development"
           href="#"
-          Icon={FiUser}
+          Icon={IoLogoAndroid}
         />
-        <Card title="Email" subtitle="Manage email" href="#" Icon={FiMail} />
-        <Card title="Team" subtitle="Manage team" href="#" Icon={FiUsers} />
+        <Card title="React" subtitle="Web Development" href="#" Icon={IoLogoReact} />
+        <Card title="Flutter" subtitle="Android app Development" href="#" Icon={SiFlutter} />
         <Card
-          title="Billing"
-          subtitle="Manage cards"
+          title="UI/UX"
+          subtitle="UI/UX development"
           href="#"
-          Icon={FiCreditCard}
+          Icon={IoLogoFigma}
         />
       </div>
     </div>
@@ -29,13 +35,13 @@ const Card = ({ title, subtitle, Icon, href }) => {
   return (
     <a
       href={href}
-      className="w-full p-4 bg-transparent rounded border-[1px] border-slate-700 relative overflow-hidden group bg-white"
+      className="w-full p-4 bg-transparent rounded border-[1px] border-slate-700 relative overflow-hidden group "
     >
       <div className="absolute inset-0 bg-gradient-background translate-y-[100%] group-hover:translate-y-[0%] transition-transform duration-300" />
 
       <Icon className="absolute z-10 -top-12 -right-12 text-9xl text-slate-500 group-hover:text-violet-400 group-hover:rotate-12 transition-transform duration-300" />
       <Icon className="mb-2 text-2xl text-violet-600 group-hover:text-slate-500 transition-colors relative z-10 duration-300" />
-      <h3 className="font-medium text-lg text-slate-950 group-hover:text-white relative z-10 duration-300">
+      <h3 className="font-medium text-lg text-gray-400 group-hover:text-white relative z-10 duration-300">
         {title}
       </h3>
       <p className="text-slate-400 group-hover:text-violet-200 relative z-10 duration-300">
