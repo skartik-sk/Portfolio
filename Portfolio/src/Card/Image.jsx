8dc94bdc6455c31c9a661daff5a660a7cc9b5fe5
Singupalli Kartik <skartik1706@gmail.com>
2024-07-22 00:29:35 +0530

    final images.jsx

--- Portfolio/src/Card/Image.jsx
+++ Portfolio/src/Card/Image.jsx
@@ -2,22 +2,25 @@ import React from "react";
 import { motion, useDeprecatedInvertedScale } from "framer-motion";
 import { closeSpring } from "./animations";
 
-export const Image = ({ id, isSelected, pointOfInterest = 0, backgroundColor }) => {
+export const Image = ({ id, isSelected, pointOfInterest, backgroundColor }) => {
   const inverted = useDeprecatedInvertedScale();
 
   return (
     <motion.div
-      className="relative overflow-hidden w-full h-[420px] transform-gpu"
+      className="relative overflow-hidden w-full h-[400px] mx-auto transform-gpu"
       style={{ ...inverted, backgroundColor, originX: 0, originY: 0 }}
     >
       <motion.img
-        className="absolute inset-0 w-full h-full object-cover"
-        src={`images/${id}.jpg`}
+        className="absolute inset-0 w-full h-full  object-cover"
+        src={`images/${id}.png`}
         alt=""
-        initial={false}
+        initial={ false}
+
         animate={
-          isSelected ? { x: -20, y: -20 } : { x: -pointOfInterest, y: 0 }
+          isSelected ? {opacity:1, x:0, y: 0 } : { x: 0, y: 50 }
+          
         }
+      
         transition={closeSpring}
       />
     </motion.div>
