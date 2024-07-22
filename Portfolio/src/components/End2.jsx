865ea0620a95486cfb86928c4deb897b5ea0ee86
Singupalli Kartik <skartik1706@gmail.com>
2024-07-22 22:34:03 +0530

    Update End2 component with design-oriented code and CTA button

---
+++ Portfolio/src/components/End2.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const End2 = () => {
+    return (
+      <div className="px-32 mb-16   ">
+        <div className="text-4xl font-bold w-7/12  text-left mb-3">
+          See the impact of good, design - oriented code on your business.
+        </div>
+        <div className="relative pb-2 inline-block">
+          <span className="text-transparent  cursor-pointer text-5xl font-bold bg-clip-text bg-gradient-background z-10 relative">
+            Let's work together, &#8599;
+          </span>
+          <div className="absolute inset-x-0 bottom-0 h-1 bg-gradient-background"></div>
+        </div>
+      </div>
+    );
+  };
+  
+
+export default End2
